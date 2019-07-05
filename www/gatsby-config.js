4f2c0027682fd7255c90371ed308c00663db0eab
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-07-05 15:14:37 -0400

    update packages

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -33,15 +33,6 @@ module.exports = {
         path: `${__dirname}/src/data/`,
       },
     },
-    // {
-    //   resolve: "gatsby-source-sanity",
-    //   options: {
-    //     projectId: process.env.SANITY_PROJECT_ID,
-    //     dataset: process.env.SANITY_DATASET,
-    //     token: process.env.SANITY_DEPLOY_STUDIO_TOKEN,
-    //     overlayDrafts: false,
-    //   },
-    // },
     "gatsby-transformer-sharp",
     "gatsby-transformer-yaml",
   ],
