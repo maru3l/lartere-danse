2c6a4a78bf2080b4cefdc49e1927cbbf30fbb0e8
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-09-02 09:31:08 -0400

    add team, board and partners to a-propos page

--- www/src/pages/a-propos.jsx
+++ www/src/pages/a-propos.jsx
@@ -10,11 +10,18 @@ import wrapper from "../utils/wrapper"
 import { graphql } from "gatsby"
 import TeamGrid from "../components/TeamGrid/TeamGrid"
 import { colors } from "../styles/variables"
+import mediaQuery from "../utils/media-query"
+import PortableText from "../components/PortableText/PortableText"
 
 const AProposPage = ({ data }) => {
   const imgAboutIntro = data.imgAboutIntro.childImageSharp.fluid
   const imgAboutMandat = data.imgAboutMandat.childImageSharp.fluid
   const imgAboutHistory = data.imgAboutHistory.childImageSharp.fluid
+  const teamMembers = data.teamMembers.edges.map(({ node }) => node)
+  const boardMembers = data.boardMembers.edges.map(({ node }) => node)
+  const partners = data.partners.edges.map(({ node }) => node)
+
+  console.log(teamMembers)
 
   return (
     <Layout orange>
@@ -33,7 +40,7 @@ const AProposPage = ({ data }) => {
           <p className="h2 color-black">
             L’Artère est un catalyseur de rencontres humaines et artistiques,
             donnant lieu à des expérimentations et des réflexions, dans un but
-            d’actualisation permanente de la discipline de l’art de la danse et
+            d’actualisation permanente de la discipline de l’art de la danse et
             du mouvement.
           </p>
         </section>
@@ -67,10 +74,10 @@ const AProposPage = ({ data }) => {
           <h2 className="h3 color-black">Mission</h2>
 
           <p className="h2">
-            Contribuer au développement et à la valorisation de l’art de la
-            danse et du mouvement sur le territoire de la Capitale-Nationale et
-            au-delà, en soutenant le perfectionnement des artistes
-            professionnel·le·s au quotidien.
+            Contribuer au développement et à la valorisation de l’art de la
+            danse et du mouvement sur le territoire de la Capitale-Nationale et
+            au-delà, en soutenant le perfectionnement des artistes
+            professionnel·le·s au quotidien.
           </p>
         </section>
 
@@ -79,25 +86,25 @@ const AProposPage = ({ data }) => {
 
           <TextColumns className="h3 color-pink">
             <p>
-              Offrir, à Québec, une programmation de stages en art de la danse
+              Offrir, à Québec, une programmation de stages en art de la danse
               et du mouvement destinés aux artistes professionnel·le·s,
               répondant aux besoins actuels de la discipline;{" "}
             </p>
 
             <p>
               Contribuer à la rétention d’artistes professionnel·le·s en art de
-              la danse et du mouvement sur le territoire de la
+              la danse et du mouvement sur le territoire de la
               Capitale-Nationale;
             </p>
 
             <p>
-              Nourrir le foisonnement et la singularité des démarches
-              artistiques en favorisant des espaces de réflexion;{" "}
+              Nourrir le foisonnement et la singularité des démarches
+              artistiques en favorisant des espaces de réflexion;{" "}
             </p>
 
             <p>
               Favoriser les rencontres intergénérationnelles et
-              interdisciplinaires entre les artistes d’ici et d’ailleurs;{" "}
+              interdisciplinaires entre les artistes d’ici et d’ailleurs;{" "}
             </p>
 
             <p>
@@ -106,17 +113,17 @@ const AProposPage = ({ data }) => {
             </p>
 
             <p>
-              Viser le développement des artistes, collectifs et compagnies de
-              la relève en étant gestionnaire de la mesure Première Ovation –
+              Viser le développement des artistes, collectifs et compagnies de
+              la relève en étant gestionnaire de la mesure Première Ovation –
               Danse, en facilitant leur intégration au milieu professionnel par
               l’attribution de différentes bourses.
             </p>
           </TextColumns>
 
           <p className="h2 color-black">
-            L’une des spécificités et singularité de la discipline de la danse
-            réside dans la nécessité pour les praticien·ne·s de cet art de
-            demeurer dans un constant perfectionnement.
+            L’une des spécificités et singularité de la discipline de la danse
+            réside dans la nécessité pour les praticien·ne·s de cet art de
+            demeurer dans un constant perfectionnement.
           </p>
 
           <TextColumns className="h3 color-black">
@@ -125,14 +132,14 @@ const AProposPage = ({ data }) => {
               depuis 2003, L’Artère joue un rôle prédominant dans l’avancement
               de la danse contemporaine à Québec en offrant les activités de
               perfectionnement aux artistes, indispensable au maintien d’une
-              carrière en danse. Étant organisme résident à la Maison pour la
+              carrière en danse. Étant organisme résident à la Maison pour la
               danse, L’Artère contribue grandement à la vitalité de ce lieu par
               sa programmation d’envergure.
             </p>
 
             <p>
               La synergie créée par l’implantation des activités de ces deux
-              organismes sous un même toit contribue vivement au renforcement de
+              organismes sous un même toit contribue vivement au renforcement de
               leurs missions respectives. Depuis janvier 2019, L’Artère a reçu
               le mandat du service de la culture de la Ville de Québec pour la
               gestion de la mesure Première Ovation — Danse, reconnaissant
@@ -142,14 +149,14 @@ const AProposPage = ({ data }) => {
 
           <TextColumns className="h3 color-black">
             <p>
-              La programmation de L’Artère est tissée soigneusement en fonction
+              La programmation de L’Artère est tissée soigneusement en fonction
               des besoins spécifiques du milieu afin d’assurer une offre de
-              perfectionnement continu sur le territoire. Elle est articulée
-              dans l’intention de contribuer à la rétention des artistes sur le
+              perfectionnement continu sur le territoire. Elle est articulée
+              dans l’intention de contribuer à la rétention des artistes sur le
               territoire, de nourrir et d’encourager l’émergence des démarches
               des créateur·rice·s, de contribuer au décloisonnement des
               pratiques et d’offrir une incursion singulière au grand public
-              dans la pratique : conférences-performances, sorties d’atelier de
+              dans la pratique : conférences-performances, sorties d’atelier de
               création, classes ouvertes.
             </p>
           </TextColumns>
@@ -160,15 +167,15 @@ const AProposPage = ({ data }) => {
               pérennité de la pratique, de partage de connaissances, d’autonomie
               dans la créativité et de valorisation de la diversité culturelle.
               L’une des lignes directrices de leur planification, autre que de
-              maintenir l’artiste en danse dans un corps physique apte à
-              entreprendre sa charge de travail quotidienne, est d’alimenter
+              maintenir l’artiste en danse dans un corps physique apte à
+              entreprendre sa charge de travail quotidienne, est d’alimenter
               rigoureusement la fibre chercheuse chez l’artiste en danse et de
               créer un espace libre et ouvert pour nourrir les démarches des
               créateur·rice·s et encourager l’émergence de nouvelles. L’Artère
               mise sur la récurrence de techniques qui, en plus d’échauffer le
-              corps, suscitent l’usage de la créativité, attirent des adeptes de
-              mouvement de tout acabit et de toute provenance et créent ainsi un
-              espace de réflexion collective foisonnant autour de la pratique en
+              corps, suscitent l’usage de la créativité, attirent des adeptes de
+              mouvement de tout acabit et de toute provenance et créent ainsi un
+              espace de réflexion collective foisonnant autour de la pratique en
               danse.
             </p>
           </TextColumns>
@@ -204,20 +211,20 @@ const AProposPage = ({ data }) => {
 
           <TextColumns className="h3 color-black">
             <p>
-              En 2003, à l’initiative de Rosalie Trudel et Judith
-              Lessard-Bérubé, un groupe de neuf diplômées de L’École de danse de
-              Québec se sont mobilisées et ont mis sur pied L’Artère,
-              coopérative de danseur·se·s professionnel·le·s de Québec. Son but
-              était alors de répondre au besoin urgent de créer un espace de
-              rassemblement pour la communauté professionnelle en danse
-              contemporaine à Québec.
+              En 2003, à l’initiative de Rosalie Trudel et Judith
+              Lessard-Bérubé, un groupe de neuf diplômées de{" "}
+              <a href="https://ledq.qc.ca/">L’École de danse de Québec</a> se
+              sont mobilisées et ont mis sur pied L’Artère, coopérative de
+              danseur·se·s professionnel·le·s de Québec. Son but était alors de
+              répondre au besoin urgent de créer un espace de rassemblement pour
+              la communauté professionnelle en danse contemporaine à Québec.
             </p>
 
             <p>
               Ainsi, le groupe a commencé à organiser des stages de danse
               contemporaine avec un désir d’ouverture et de développement pour
-              la communauté. Cette programmation, toujours plus à la pointe de
-              la pratique de la danse, a contribué et contribue encore à la
+              la communauté. Cette programmation, toujours plus à la pointe de
+              la pratique de la danse, a contribué et contribue encore à la
               rétention d’artistes de grand talent sur le territoire en
               favorisant une création et une production locale, ainsi que
               l’employabilité des artistes de ce secteur.
@@ -236,18 +243,30 @@ const AProposPage = ({ data }) => {
 
             <p>
               Notre organisme constitue désormais un pilier indispensable à
-              l’équilibre de ce milieu. Le Plan directeur de la danse
-              professionnelle au Québec 2011-2021 et le Plan de développement de
-              la danse professionnelle à Québec en soulignent d’ailleurs
-              l’importance.
+              l’équilibre de ce milieu.{" "}
+              <a href="https://www.quebecdanse.org/a-propos/plan-directeur/">
+                Le Plan directeur de la danse professionnelle au Québec
+                2011-2021
+              </a>{" "}
+              et{" "}
+              <a href="https://www.quebecdanse.org/wp-content/uploads/2018/11/danseqc_plan_dev_v16mars20121.pdf">
+                le Plan de développement de la danse professionnelle à Québec
+              </a>
+              en soulignent d’ailleurs l’importance.
             </p>
 
             <p>
-              Aujourd’hui, résidente permanente à la Maison pour la danse et
-              responsable de la mesure Première Ovation — Danse, L’Artère
-              continue d’affirmer et d’assumer son rôle de précurseur, en
-              prenant entièrement en charge l’offre d’activités de
-              perfectionnement en art de la danse et du mouvement à Québec de
+              Aujourd’hui, résidente permanente à la{" "}
+              <a href="http://www.maisonpourladanse.ca/">
+                Maison pour la danse
+              </a>{" "}
+              et responsable de la mesure{" "}
+              <a href="https://premiereovation.com/">
+                Première Ovation — Danse
+              </a>
+              , L’Artère continue d’affirmer et d’assumer son rôle de
+              précurseur, en prenant entièrement en charge l’offre d’activités
+              de perfectionnement en art de la danse et du mouvement à Québec de
               façon organisée et professionnelle, mettant ainsi à profit toute
               l’expertise développée au fil des ans.
             </p>
@@ -256,17 +275,17 @@ const AProposPage = ({ data }) => {
           <TextColumns className="h3">
             <p>
               <u>
-                Membres fondatrices de L’Artère, coopérative de danseurs
-                professionnels de Québec :
+                Membres fondatrices de L’Artère, coopérative de danseurs
+                professionnels de Québec :
               </u>{" "}
-              Maryse Bonenfant, Céline Khandjian, Karine Ledoyen, Judith
-              Lessard-Bérubé, Sonia Montminy, Karine Parisé, Mélanie Therrien,
+              Maryse Bonenfant, Céline Khandjian, Karine Ledoyen, Judith
+              Lessard-Bérubé, Sonia Montminy, Karine Parisé, Mélanie Therrien,
               Rosalie Trudel et Arielle Warnke St-Pierre.
             </p>
 
             <p>
               <u>
-                Membres fondateur·rice·s de l’OBNL, L’Artère, développement et
+                Membres fondateur·rice·s de l’OBNL, L’Artère, développement et
                 perfectionnement en danse contemporaine :
               </u>{" "}
               Maryse Damecour, Anne-Pier Dion, Annie Gagnon, Isabelle Gagnon,
@@ -308,19 +327,19 @@ const AProposPage = ({ data }) => {
               max-width: 1000px;
             `}
           >
-            Un organisme dirigé par les artistes!
+            Un organisme dirigé par les artistes!
           </p>
 
           <TextColumns className="h3 color-black">
             <p>
-              La force et la distinction de L’Artère sont tributaires du
+              La force et la distinction de L’Artère sont tributaires du
               dévouement des artistes qui l’ont forgé : un organisme pour la
               communauté, par la communauté. La structure organisationnelle de
               L’Artère est réfléchie dans un esprit collaboratif et tissée
               soigneusement en résonance avec les besoins du milieu. Elle met à
               profit l’apport indispensable des artistes du milieu en les
               impliquant via des comités de travail (artistique, services aux
-              membres, financement privé) ou en les employant pour différentes
+              membres, financement privé) ou en les employant pour différentes
               tâches (coordination Lieux communs et SIA — Performance), tout en
               étant encadrés par une coordination générale. De la sorte,
               L’Artère valorise et reconnaît les compétences de ses membres,
@@ -336,6 +355,7 @@ const AProposPage = ({ data }) => {
           <h2 className="h3 color-black">L’équipe de L’Artère</h2>
 
           <TeamGrid
+            members={teamMembers}
             css={css`
               color: ${colors.Jet};
             `}
@@ -346,6 +366,7 @@ const AProposPage = ({ data }) => {
           <h2 className="h3 color-black">Conseil d’administration</h2>
 
           <TeamGrid
+            members={boardMembers}
             css={css`
               color: ${colors.Jet};
             `}
@@ -366,6 +387,52 @@ const AProposPage = ({ data }) => {
             compte sur des collaborations entretenues depuis plusieurs années
             avec des organismes homologues canadiens.
           </p>
+
+          <ul
+            css={css`
+              list-style: none;
+              margin: 0;
+              padding: 0;
+
+              columns: 1;
+              margin-top: ${100 / 55}em;
+              margin-bottom: ${200 / 55}em;
+
+              ${mediaQuery.greaterThen(768)} {
+                columns: 2;
+              }
+
+              ${mediaQuery.greaterThen(1024)} {
+                columns: 3;
+              }
+            `}
+          >
+            {partners.map(partner => (
+              <li
+                css={css`
+                  break-inside: avoid;
+                  margin: 0 0 1em;
+                `}
+              >
+                <p
+                  css={css`
+                    color: ${colors.Jet};
+                    margin-bottom: 0.5em;
+                  `}
+                >
+                  {partner.name}
+                </p>
+
+                <div
+                  css={css`
+                    margin-top: 0.5em;
+                  `}
+                >
+                  <PortableText blocks={partner._rawDescription} />
+                </div>
+              </li>
+            ))}
+          </ul>
         </section>
       </article>
     </Layout>
@@ -385,6 +452,7 @@ export const query = graphql`
         }
       }
     }
+
     imgAboutMandat: file(relativePath: { eq: "img-about-mandat.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 1800, quality: 90) {
@@ -394,12 +462,41 @@ export const query = graphql`
         }
       }
     }
+
     imgAboutHistory: file(relativePath: { eq: "img-about-history.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 1800, quality: 90) {
           src
           srcSet
           srcSetWebp
+        }
+      }
+    }
+
+    partners: allSanityPartner(sort: { fields: order, order: ASC }) {
+      edges {
+        node {
+          _rawDescription
+          name
+        }
+      }
+    }
+
+    boardMembers: allSanityBoard(sort: { fields: order, order: ASC }) {
+      edges {
+        node {
+          name
+          role
+        }
+      }
+    }
+
+    teamMembers: allSanityTeam(sort: { fields: order, order: ASC }) {
+      edges {
+        node {
+          name
+          role
+          email
         }
       }
     }
