e33909f3385dcdbed9484dd94ffbe04ffbef43e3
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-09-04 21:08:33 -0400

    create Calendar component

---
+++ www/src/components/Calendar/Calendar.jsx
@@ -0,0 +1,78 @@
+// vendors
+import React, { useState } from "react"
+import Dates from "./Dates"
+import css from "@emotion/css"
+import styled from "@emotion/styled"
+import { colors } from "../../styles/variables"
+
+const SwitcherButton = styled.button`
+  appearance: none;
+  border: none;
+  background: none;
+  color: inherit;
+`
+
+const Calendar = ({
+  month = 9,
+  year = 2019,
+  switcher = false,
+  events = [],
+}) => {
+  const [currentMonth, setCurrentMonth] = useState(month)
+  const [currentYear, setCurrentYear] = useState(year)
+
+  const increaseMonth = () => {
+    if (currentMonth > 11) {
+      setCurrentMonth(1)
+
+      setCurrentYear(currentYear + 1)
+
+      return
+    }
+
+    setCurrentMonth(currentMonth + 1)
+  }
+  const decreaseMonth = () => {
+    if (currentMonth < 2) {
+      setCurrentMonth(12)
+
+      setCurrentYear(currentYear - 1)
+
+      return
+    }
+
+    setCurrentMonth(currentMonth - 1)
+  }
+
+  const date = new Date(currentYear, currentMonth - 1, 1, 0, 0, 0)
+  return (
+    <div
+      css={css`
+        display: grid;
+        grid-template-columns: repeat(7, 1fr);
+        grid-auto-rows: 1fr;
+      `}
+    >
+      <div
+        css={css`
+          grid-row: 1 / span 1;
+          grid-column: 1 / span 3;
+          text-align: center;
+          color: ${colors.grey};
+        `}
+      >
+        {switcher && (
+          <SwitcherButton onClick={decreaseMonth}>{"<"}</SwitcherButton>
+        )}
+        {date.toLocaleDateString("fr", { month: "long" })}
+        {switcher && (
+          <SwitcherButton onClick={increaseMonth}>{">"}</SwitcherButton>
+        )}
+      </div>
+
+      <Dates month={currentMonth - 1} year={currentYear} events={events} />
+    </div>
+  )
+}
+
+export default Calendar
---
+++ www/src/components/Calendar/Dates.jsx
@@ -0,0 +1,141 @@
+// vendors
+import React from "react"
+import css from "@emotion/css"
+import { fonts, colors } from "../../styles/variables"
+import { keyframes } from "@emotion/core"
+
+const getColorForAudience = audience => {
+  switch (audience) {
+    case "professionnel":
+      return colors.PortlandOrange
+    case "sceneArtist":
+      return colors.canary
+    case "artist":
+      return colors.PaleCerulean
+    case "generalPublic":
+      return colors.grey
+    default:
+      return "#fff"
+  }
+}
+
+const Event = ({ event }) => {
+  const delta = 100 / (event.targetAudience.length + 1)
+
+  const animateLink = keyframes`
+    ${event.targetAudience.map(
+      (audience, index) => css`
+        ${delta * index}% {
+          color: ${getColorForAudience(audience)};
+        }
+
+        ${index === 0 &&
+          css`
+            100% {
+              color: ${getColorForAudience(audience)};
+            }
+          `}
+      `
+    )}
+  `
+
+  return (
+    <li
+      css={css`
+        margin-top: 0.5em;
+      `}
+    >
+      <a
+        css={css`
+          text-decoration: none;
+          animation: ${animateLink} ${delta * 0.25}s infinite;
+
+          :hover {
+            animation: none;
+            color: ${colors.pink};
+          }
+        `}
+        href={`/activites#${event.slug}`}
+      >
+        {event.title}
+      </a>
+    </li>
+  )
+}
+
+const Dates = ({ month, year, events = [] }) => {
+  const daysInMonth = new Date(year, month + 1, 0, 0, 0, 0).getUTCDate()
+  const firstDayInMonthPosition = new Date(year, month, 1, 0, 0, 0).getUTCDay()
+
+  return (
+    <>
+      {Array(daysInMonth)
+        .fill()
+        .map((val, index) => (
+          <div
+            css={css`
+              padding-bottom: 100%;
+              position: relative;
+
+              ${index === 0 &&
+                css`
+                  grid-column: ${firstDayInMonthPosition + 1};
+                `}
+            `}
+          >
+            <div
+              css={css`
+                position: absolute;
+                top: 0;
+                right: 0;
+                bottom: 0;
+                left: 0;
+                overflow: hidden;
+                z-index: 1;
+                height: 100%;
+              `}
+            >
+              <ul
+                css={css`
+                  min-height: 100%;
+                  display: flex;
+                  flex-flow: column;
+                  justify-content: center;
+                  font-size: ${27 / 33}em;
+                  list-style: none;
+                  margin: 0;
+                  padding: 0;
+                  margin-right: 1em;
+                `}
+              >
+                {events
+                  .filter(event => {
+                    const today = new Date(year, month, index + 1, 0, 0, 0)
+
+                    return today.getTime() === event.date.getTime()
+                  })
+                  .map(event => (
+                    <Event event={event} />
+                  ))}
+              </ul>
+            </div>
+
+            <time
+              className="h2"
+              css={css`
+                position: absolute;
+                color: #000;
+                margin-left: 0.25em;
+                font-family: ${fonts.accent};
+              `}
+              dateTime={`${year}-${month + 1}-${index + 1}`}
+            >
+              {index + 1}
+            </time>
+          </div>
+        ))}
+    </>
+  )
+}
+
+export default Dates
