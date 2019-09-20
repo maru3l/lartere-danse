7b782550310627acba494d7817f828185b930263
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-09-20 12:11:03 -0400

    create useMobile hook

---
+++ www/src/hooks/useMobile.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react"
+
+const windowWidth = () => {
+  return window ? window.innerWidth : 0
+}
+
+export default (breakpoint = 1024) => {
+  const [isMobile, setIsMobile] = useState(windowWidth() < breakpoint)
+
+  const handleResize = () => {
+    const width = windowWidth()
+
+    setIsMobile(width < breakpoint)
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize)
+
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  })
+
+  return isMobile
+}
