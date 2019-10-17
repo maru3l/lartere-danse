6d63124d403494391d9d200859189a541e1798a2
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-10-17 10:56:57 -0400

    order event schema by eventType

---
+++ sanity/deskStructure.js
@@ -0,0 +1,32 @@
+import S from "@sanity/desk-tool/structure-builder";
+
+const hiddenDocTypes = listItem => ![
+  "eventType",
+  "event"
+].includes(listItem.getId())
+
+export default () =>
+  S.list()
+    .title("Content")
+    .items([
+      // S.listItem()
+      //   .title('config'),
+      S.listItem()
+        .title("Activités")
+        .child(
+          S.documentList()
+            .title("Catégories d'activité")
+            .menuItems(S.documentTypeList("eventType").getMenuItems())
+            .filter('_type == $type && !defined(parents)')
+            .params({ type: 'eventType' })
+            .child(categoryId => {
+              return S.documentList()
+                .title('Activités')
+                .menuItems(S.documentTypeList('event').getMenuItems())
+                .filter('_type == $type && eventType._ref == $categoryId')
+                .params({ type: 'event', categoryId })}
+              )
+        ),
+      ...S.documentTypeListItems()
+        .filter(hiddenDocTypes)
+    ]);
