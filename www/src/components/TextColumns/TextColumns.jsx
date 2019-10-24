bc597928026ff3958ab6b17620fce6e56b6b5862
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-10-24 15:01:40 -0400

    move rule for no margin in column when 2 column only

--- www/src/components/TextColumns/TextColumns.jsx
+++ www/src/components/TextColumns/TextColumns.jsx
@@ -10,18 +10,18 @@ const TextColumns = ({ columns = 2, children, ...props }) => {
         columns: 1;
         column-gap: 1.5em;
 
-        *:first-child {
-          margin-top: 0px;
-        }
-
-        h2 + &,
-        h3 + &,
-        p + & {
-          margin-top: 0;
-        }
-
         ${mediaQuery.greaterThen(768)} {
           columns: ${columns};
+
+          *:first-child {
+            margin-top: 0px;
+          }
+
+          h2 + &,
+          h3 + &,
+          p + & {
+            margin-top: 0;
+          }
         }
       `}
       {...props}
