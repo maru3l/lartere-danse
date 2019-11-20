d11cfb8f8da0ee323a18c136a7da8e160a598710
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-11-19 20:00:33 -0500

    repair date format in EventCard

--- www/src/components/EventCard/EventCard.jsx
+++ www/src/components/EventCard/EventCard.jsx
@@ -8,33 +8,9 @@ import Collapse from "../Collapse/Collapse"
 import getByTerm from "../../../../utils/getByTerm"
 import Button from "../Button/Button"
 import { linkStyle } from "../../styles/global"
+import DateCard from "./DateCard"
 
 const EventCard = ({ event, small = false, ...props }) => {
-  let fromDate = null
-  let toDate = null
-  let hours = []
-
-  if (event.date && event.date.length > 0) {
-    fromDate = new Date(
-      Math.min.apply(
-        null,
-        event.date.map(({ from }) => new Date(`${from} 00:00:00`))
-      )
-    )
-    toDate = new Date(
-      Math.min.apply(
-        null,
-        event.date.map(({ to }) => new Date(`${to} 00:00:00`))
-      )
-    )
-    hours = event.date.reduce((acc, { fromTime, toTime }) => {
-      const found = acc.find(
-        ({ fromTime: from, toTime: to }) => fromTime === from && toTime === to
-      )
-      return found ? acc : [...acc, { fromTime, toTime }]
-    }, [])
-  }
-
   return (
     <article
       id={event.slug.current}
@@ -109,114 +85,22 @@ const EventCard = ({ event, small = false, ...props }) => {
             </h1>
           )}
 
-          {/* {event.targetAudience.length > 0 && (
-            <section>
-              <VisuallyHidden>
-                <h2>Public cible</h2>
-              </VisuallyHidden>
-
-              <ul
-                css={css`
-                  margin-top: 0;
-                  margin-bottom: 1em;
-                  padding: 0;
-                  list-style: none;
-
-                  li {
-                    display: inline-block;
-                    :not(:last-child):after {
-                      content: "+";
-                      margin: 0 0.35ch;
-                      color: ${colors.text};
-                    }
-                  }
-
-                  abbr {
-                    text-decoration: none;
-                  }
-                `}
-              >
-                {event.targetAudience.includes("professionnel") && (
-                  <li className="color-orange">
-                    <abbr title="Professionnel·le·s des arts de la danse et du mouvement">
-                      P
-                    </abbr>
-                  </li>
-                )}
-
-                {event.targetAudience.includes("sceneArtist") && (
-                  <li className="color-canary">
-                    <abbr title="Bougeur·se·s expérimenté·e·s (cirque, arts martiaux, danse, etc.)">
-                      B
-                    </abbr>
-                  </li>
-                )}
-
-                {event.targetAudience.includes("artist") && (
-                  <li className="color-pale-cerulean">
-                    <abbr title="Artistes des arts de la scène (théâtre, musique, perfomance, cinéma)">
-                      A
-                    </abbr>
-                  </li>
-                )}
-
-                {event.targetAudience.includes("generalPublic") && (
-                  <li>
-                    <abbr title="Grand public">GP</abbr>
-                  </li>
-                )}
-              </ul>
-            </section>
-          )} */}
-
           <section>
             <VisuallyHidden>
               <h2>Date et horaire</h2>
             </VisuallyHidden>
 
             {event.date && (
-              <div>
-                <p
-                  css={css`
-                    margin-bottom: 0;
-                  `}
-                >
-                  {fromDate.getDate()}{" "}
-                  {fromDate.getMonth() !== toDate.getMonth() &&
-                    fromDate.toLocaleDateString("fr", { month: "long" })}{" "}
-                  {fromDate.getYear() !== toDate.getYear() &&
-                    fromDate.getFullYear()}{" "}
-                  {fromDate.getTime() !== toDate.getTime() && <>au</>}{" "}
-                  {fromDate.getTime() !== toDate.getTime() && toDate.getDate()}{" "}
-                  {toDate.toLocaleDateString("fr", { month: "long" })}{" "}
-                  {toDate.getFullYear()}
-                </p>
-
-                {hours.length > 0 && (
-                  <ul
-                    css={css`
-                      list-style: none;
-                      padding: 0;
-
-                      li:not(:last-child):after {
-                        content: "+";
-                        margin: 0.5ch;
-                      }
-                    `}
-                  >
-                    {hours.map(time => (
-                      <li>
-                        {time.fromTime.hour}h
-                        {time.fromTime.minute > 0 && time.fromTime.minute} à{" "}
-                        {time.toTime.hour}h
-                        {time.toTime.minute > 0 && time.toTime.minute}
-                      </li>
-                    ))}
-                  </ul>
-                )}
-
-                {hours.length < 1 && <p>Horaire à venir</p>}
-              </div>
+              <ul
+                css={css`
+                  list-style: none;
+                  padding: 0;
+                `}
+              >
+                {event.date.map(date => (
+                  <DateCard date={date} />
+                ))}
+              </ul>
             )}
 
             {!event.date && (
