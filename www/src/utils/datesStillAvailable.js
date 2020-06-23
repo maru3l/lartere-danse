787b97cadfcb31ad61a3df44918d1bdfc8f9425d
Marc-Antoine Ruel <hello@marcantoineruel.com>
2020-06-23 08:06:31 -0400

    repair date evaluation

--- www/src/utils/datesStillAvailable.js
+++ www/src/utils/datesStillAvailable.js
@@ -1,9 +1,14 @@
 const now = new Date()
 
 module.exports = dates => {
-  const oldest = dates.reduce((acc, cur) =>
-    Date.parse(acc.to) > Date.parse(cur.to) ? acc : cur
-  )
+  const oldest = dates.reduce((acc, cur) => {
+    const dateToEvaluateAcc = acc.to || acc.from
+    const dateToEvaluateCur = cur.to || cur.from
+
+    return Date.parse(dateToEvaluateAcc) > Date.parse(dateToEvaluateCur)
+      ? acc
+      : cur
+  })
 
   return now.getTime() < Date.parse(oldest.to)
 }
